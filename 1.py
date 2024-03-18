2cbcf51d76d0817c1f545d1b67ae8a73811c79bd
Pradeep-Deep14 <123350286+Pradeep-Deep14@users.noreply.github.com>
2024-03-18 11:09:22 +0530

    first commit

---
+++ 1.py
@@ -0,0 +1,9 @@
+#Number of Unique elements in list    
+
+List=['Apple','Banana','Banana','Guava']
+
+Unique_Elements = len(list(set(List)))
+
+count=(Unique_Elements)
+
+print("The No of unique elements in the list is:",count)
